ef8a934f2a98c674001b2170998d85e1395064ab
Sen Cai <sencaichi@gmail.com>
2023-07-08 10:34:43 -0400

    Initial Commit

---
+++ Pill'd/Pill_dApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pill_dApp.swift
+//  Pill'd
+//
+//  Created by Sen Ayané on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Pill_dApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
