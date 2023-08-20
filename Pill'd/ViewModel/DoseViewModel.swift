e20811a29f6c9de2f66aeaeddb62f61cc99fbd06
Sen Cai <sencaichi@gmail.com>
2023-08-19 21:05:00 -0400

    more work on data stuff, Persistence file added

---
+++ Pill'd/ViewModel/DoseViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  MedicationForm.swift
+//  Pill'd
+//
+//  Created by Sen Ayané on 7/10/23.
+//
+
+import SwiftUI
+import CoreData
+//
+//class DoseViewViewModel: ObservableObject {
+//    @Published var openNewDose: Bool = false
+//    @Published var doseDateTime: Date = Date()
+//    @Published var doseMedication: Medication = Medication()
+//
+//    func addDose(context: NSManagedObjectContext){
+//        let dose = Dose(context: context)
+//        dose.dateTime = doseDateTime
+////        dose.medication
+//    }
+//}
